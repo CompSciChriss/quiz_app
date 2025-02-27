bf1efd6c5beac2743eda50eac22971bd1f787696
Christopher Dorin <chrisdorin45@gmail.com>
2025-02-27 12:20:01 -0500

    Added Google Fonts & Shuffled Answers

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -12,14 +12,17 @@ class AnswerButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
-        backgroundColor:const Color. fromARGB(255, 33, 1, 95),
-        foregroundColor:Colors.white,
-        padding: const EdgeInsets. symmetric(
-          vertical: 10, 
+        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        foregroundColor: Colors.white,
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
           horizontal: 40,
-        ), // EdgeInsets. symmetric
+        ),
       ),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,28 +13,43 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((item){
-            return AnswerButton(answerText: item, onTap: (){},);
-          }),
-        ],
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((item) {
+              return AnswerButton(
+                answerText: item,
+                onTap: answerQuestion,
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
